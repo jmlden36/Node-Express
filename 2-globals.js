f0ba92d713a85502a9baf7b62b2047230dd5a242
Matt Luker <jamesmattluker@gmail.com>
2022-05-02 12:36:44 -0600

    add some starter files

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// GLOBALS no Windo
+// examples of globals
+// __dirname - path to current directory
+// __filename - file name
+// require      - function to use modules (CommonJS)
+// module    - info about current module (file)
+// process   - info about environment where the program is being executed
+// 		THIS IS AN IMPORTANT ONE 
+
+console.log(process);
+// prints info about env where code is executed
